479b731aa624d3588e425ecea2c5c698fb058874
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-19 04:10:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.to_s.capitalize!}:"
     data.each do |holiday, list|
-      
         puts "  #{holiday.to_s.capitalize!}: #{list.join(", ")}"
     end
   end
