0cf8492ee41acfecbb8b3728a7e933e9fb375c6e
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-19 04:03:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |day, list|
       if list.include?("BBQ")
-        return day.keys
+        puts day.keys
       end
     end
   end
